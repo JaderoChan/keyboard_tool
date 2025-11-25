48f864798a22133be9179ab63773fae1ba7e9e60
JaderoChan <c_dl_cn@outlook.com>
2025-11-25 21:44:27 +0800

    update

--- example/echo/main.c
+++ example/echo/main.c
@@ -16,7 +16,7 @@ static void event_handler(struct keyboard_event* event)
     switch (event->type)
     {
         case KBDET_PRESSED:
-            printf("Key pressed: %s\n", kbdt_keyboard_key_to_str(key));
+            printf("Key pressed: %s!\n", kbdt_keyboard_key_to_str(key));
             if (key == KBDKEY_ESC)
             {
                 pthread_mutex_lock(&g_mtx);
@@ -26,7 +26,7 @@ static void event_handler(struct keyboard_event* event)
             }
             break;
         case KBDET_RELEASED:
-            printf("Key released: %s\n",  kbdt_keyboard_key_to_str(key));
+            printf("Key released: %s!\n", kbdt_keyboard_key_to_str(key));
             break;
         default:
             break;
@@ -38,18 +38,18 @@ int main()
     int rc = kbdt_start();
     if (rc != KBDT_RC_SUCCESS)
     {
-        printf("Failed to run kbdtool. Error code: %d\n", rc);
+        printf("Failed to run the kbdtool. Error code: %d.\n", rc);
         exit(1);
     }
 
     rc = kbdt_set_event_handler(&event_handler);
     if (rc != KBDT_RC_SUCCESS)
     {
-        printf("Failed to set event handler. Error code: %d\n", rc);
+        printf("Failed to set the event handler. Error code: %d.\n", rc);
         exit(1);
     }
 
-    printf("Press ESC to exit\n\n");
+    printf("Press ESC to exit!\n\n");
 
     pthread_mutex_lock(&g_mtx);
     while (!g_should_close)
@@ -58,6 +58,6 @@ int main()
 
     kbdt_stop();
 
-    printf("Success to exit\n");
+    printf("Success to exit!\n");
     return 0;
 }
